baa8a2385c6af8e6a76feb9b74f2190c82a7007e
Sathweeka Patnaik <91815444+Sathweeka@users.noreply.github.com>
2023-03-27 17:34:04 +0530

    Add files via upload

---
+++ DSA1(Stack).py
@@ -0,0 +1,64 @@
+'''
+#Stack
+it is a linear data structure.
+datastructure with Last-in-First-out(LIFO) behaviour
+#operations
+1.isempty
+2.isfull
+3.top
+4.push
+5.pop
+'''
+class Stack:
+    def __init__(self,max_size):
+        self.__max_size=max_size
+        self.__elements=[None]*self.__max_size#[None,None,None,None]
+        self.__top=-1
+    def is_full(self):
+        if(self.__top==self.__max_size-1):
+            return True
+        return False
+    def is_empty(self):
+        if(self.__top==-1):
+            return True
+        return False
+    def push(self,data):
+        if(self.is_full()):
+           print("The stack is full!!")
+        else:
+            self.__top+=1
+            self.__elements[self.__top]=data
+    def pop(self):
+        if(self.is_empty()):
+           print("The stack is empty!!")
+        else:
+            data=self.__elements[self.__top]
+            self.__top-=1
+            return data
+    def display(self):
+        if(self.is_empty()):
+           print("The stack is empty!!")
+        else:
+            index=self.__top
+            while(index>=0):
+                print(self.__elements[index])
+                index-=1
+    def get_max_size(self):
+        return self.__max_size
+ball_stack=Stack(4)
+print("Is it empty",ball_stack.is_empty())
+ball_stack.push(5)
+print("Is it empty",ball_stack.is_empty())
+ball_stack.push(6)
+ball_stack.push(7)
+ball_stack.push(8)
+ball_stack.display()
+print("size of the stack",ball_stack.get_max_size())
+print("the element deleted",ball_stack.pop())
+print("after deleting the element")
+ball_stack.display()
+print("size of the stack",ball_stack.get_max_size())
+           
+#-------------------------------------------------------------
+
+        
