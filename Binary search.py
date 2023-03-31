f1c3beaa7a7ffdad23b626bb622c39f4c70748c8
Sathweeka Patnaik <91815444+Sathweeka@users.noreply.github.com>
2023-03-31 20:47:50 +0530

    Add files via upload

---
+++ Binary search.py
@@ -0,0 +1,19 @@
+#Binary searchin python
+def binarySearch(array, x, low, high):
+    #repeat until the pointers low andhigh meet each other
+    while low <= high:
+        mid = low + (high - low)//2
+        if array[mid] == x:
+            return mid
+        elif array[mid] < x:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+array = [3, 4, 5, 6, 7, 8, 9]
+x = 40
+result = binarySearch(array, x, 0, len(array)-1)
+if result != -1:
+    print("Element is present at index " + str(result))
+else:
+    print("Not found")
